6cf54fa312d46ac7b06e91ed16827f241be08d46
Josh Breslow <jbreslow@ucsd.edu>
2022-03-04 10:53:00 -0800

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -18,7 +18,12 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
 		/>
       </label>
 		<div className="title">
-			<input type="text" value={title} readOnly={true} placeholder="Input title" />
+			<input 
+				type="text" 
+				value={title} 
+				readOnly={true} 
+				placeholder="Input title"
+				style={{ background: 'red' }} />
 		</div>
 
 		<div className="actions" onClick={event => event.stopPropagation()}>
